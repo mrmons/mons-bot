0fe0575ffe632f8f56d1d36f82f08f0c31b38579
vincentsoo <vincentsoo@vincents-MacBook-Air.local>
2016-07-02 07:51:50 +0800

    change code

--- app.js
+++ app.js
@@ -1,28 +1,119 @@
 'use strict'
-const http = require('http')
-const Bot = require('messenger-bot')
 
-let bot = new Bot({
-  token: 'EAACZB9QJfU0QBAPjr0V0hF4a8aGeHIF0gAvxYttmCWF0wq4ZB9ZC9zyM4HQUG3jXq7IkZBZC2Dt6hqARmpVb1M6DDrOMbx2bQfJk5a8AOoQJZCLxlrxNFGZA4ZA5EPy7HZBGNG5xGRZCJUIvGrRrnlAqIm3pnDa9EXCg4ejc9ed4fx4QZDZD',
-  verify: 'verify_token'
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
 })
 
-bot.on('error', (err) => {
-  console.log(err.message)
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'verify_token') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
 })
 
-bot.on('message', (payload, reply) => {
-  let text = payload.message.text
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
 
-  bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) throw err
+const token = "EAACZB9QJfU0QBAPjr0V0hF4a8aGeHIF0gAvxYttmCWF0wq4ZB9ZC9zyM4HQUG3jXq7IkZBZC2Dt6hqARmpVb1M6DDrOMbx2bQfJk5a8AOoQJZCLxlrxNFGZA4ZA5EPy7HZBGNG5xGRZCJUIvGrRrnlAqIm3pnDa9EXCg4ejc9ed4fx4QZDZD"
 
-    reply({ text }, (err) => {
-      if (err) throw err
-
-      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
     })
-  })
-})
+}
 
-http.createServer(bot.middleware()).listen(process.env.PORT || 5000)
+function sendGenericMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
